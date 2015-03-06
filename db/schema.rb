01c4f34e41e753c624b001ea0222ef44d2bdcfa9
Dylan Dechant <dylan.dechant@gmail.com>
2015-03-06 12:49:14 -0500

    add some user address information

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305231802) do
+ActiveRecord::Schema.define(version: 20150306170553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 20150305231802) do
     t.text     "tokens"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "address"
+    t.integer  "zip"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zpid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
