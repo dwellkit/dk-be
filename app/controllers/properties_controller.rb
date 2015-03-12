59a0c309597dbdf59ee41f9b3f2cd096646a9544
Ian Gawronski <iangawronski@gmail.com>
2015-03-12 17:19:03 -0400

    rename files, redo routes

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,4 @@
-class PropertyController < ApplicationController
+class PropertiesController < ApplicationController
   before_action :authenticate_user_from_token!
   before_action :set_property, only: [:edit, :reimport, :destroy]
 
