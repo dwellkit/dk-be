27babe0af2e2a345229a6a528d084d0804243dc5
Dylan Dechant <dylan.dechant@gmail.com>
2015-03-12 21:04:44 -0400

    rake and merge baby, rake and merge...

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150312150416) do
+ActiveRecord::Schema.define(version: 20150312210753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
