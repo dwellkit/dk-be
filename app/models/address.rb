f9f04a37ee7523fec050e2cdead689fdc6b2e12c
Dylan Dechant <dylan.dechant@gmail.com>
2015-03-14 13:35:57 -0400

    validation of address, added route to get specific property

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,18 @@ class Address < ActiveRecord::Base
   belongs_to :property
   belongs_to :contact
   has_many :users, through: :properties
+  validates_uniqueness_of :street_address, :scope => [:user_id, :zipcode], :case_sensitive => false
+
+  def add_location(location)
+    street = location[:street_address].gsub('+', ' ')
+    street = street.split.map(&:capitalize).join(' ')
+    city = location[:city].split.map(&:capitalize).join(' ')
+    state = location[:state].upcase
+
+    self.street_address = street
+    self.zipcode = location[:zipcode]
+    self.city = city
+    self.state = state
+    self.property_id = self.id
+  end
 end
