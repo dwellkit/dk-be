e740b335e594f432ca26c03ead600d688441827c
Ian Gawronski <iangawronski@gmail.com>
2015-03-17 19:25:33 -0400

    mailers working, or so we think

--- app/jobs/event_notify_job.rb
+++ app/jobs/event_notify_job.rb
@@ -1,18 +1,18 @@
 class EventNotifyJob < ActiveJob::Base
-  queue :default
+  queue_as :default
 
   def perform(event, urgency)
     case (urgency)
-    when :day_of
+    when 'day_of'
       EventMailer.reminder_day_of_email(event).deliver_now
       new_event = event.event_date + event.event_frequency.seconds
-      EventNotifyJob.perform_at(@event.event_date - 5.days, @event, :soon)
-      EventNotifyJob.perform_at(@event.event_date - 1.day, @event, :imminent)
-      EventNotifyJob.perform_at(@event.event_date, @event, :day_of)
+      EventNotifyJob.set(wait_until: event.event_date - 5.days).perform_later(event, :soon)
+      EventNotifyJob.set(wait_until: event.event_date - 1.day).perform_later(event, :imminent)
+      EventNotifyJob.set(wait_until: event.event_date_.perform_later(event, :day_of)
       event.update(event_date: new_event)
-    when :soon
+    when 'soon'
       EventMailer.reminder_soon_email(event).deliver_now
-    when :imminent
+    when 'imminent'
       EventMailer.reminder_imminent_email(event).deliver_now
     end
   end
