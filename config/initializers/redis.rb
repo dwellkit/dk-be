b077c6aab87fe5cf4418d0a5c16d3dfe2618e841
Ian Gawronski <iangawronski@gmail.com>
2015-03-18 12:14:00 -0400

    redis ish

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,5 @@
-uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
-REDIS = Redis.new(:url => uri)
+# uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
+# REDIS = Redis.new(:url => uri)
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
