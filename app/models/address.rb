596d71bd74bc7548bd9e2938dde473ff8284f948
Dylan Dechant <dylan.dechant@gmail.com>
2015-03-18 09:55:44 -0400

    wtf

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,19 +7,18 @@ class Address < ActiveRecord::Base
 
   def add_location(location)
     parsed = StreetAddress::US.parse(location[:address])
-    
-    self.full_address = parsed.to_s
-    self.state = parsed.state
-    self.city = parsed.city
-    self.postal_code = parsed.postal_code
-    self.number = parsed.number
-    self.postal_code_ext = parsed.postal_code_ext
-    self.prefix = parsed.prefix
-    self.street = parsed.street
-    self.street_type = parsed.street_type
-    self.suffix = parsed.suffix
-    self.unit = parsed.unit
-    self.unit_prefix = parsed.unit_prefix
+    self.full_address = parsed.to_s || 0
+    self.state = parsed.state || 0
+    self.city = parsed.city || 0
+    self.postal_code = parsed.postal_code || 0
+    self.number = parsed.number || 0
+    self.postal_code_ext = parsed.postal_code_ext || 0
+    self.prefix = parsed.prefix || 0
+    self.street = parsed.street || 0
+    self.street_type = parsed.street_type || 0
+    self.suffix = parsed.suffix || 0
+    self.unit = parsed.unit || 0
+    self.unit_prefix = parsed.unit_prefix || 0
     # street = location[:street_address].gsub('+', ' ')
     # street = street.split.map(&:capitalize).join(' ')
     # city = location[:city].split.map(&:capitalize).join(' ')
