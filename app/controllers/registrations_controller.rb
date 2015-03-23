3e1d00c8f1c17e74ba1fea37048da03d3308737f
Dylan Dechant <dylan.dechant@gmail.com>
2015-03-23 15:47:30 -0400

    Disable user registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,12 +4,14 @@ class RegistrationsController < Devise::RegistrationsController
       render json: { :error => "Passwords do not match" }, status: :bad_request
       return false
     end
-    @user = User.new(user_params)
-    if @user.save
-      render json: { :user => @user }, status: :created
-    else
-      render json: { :messages => @user.errors.full_messages }, status: :unprocessable_entity
-    end
+    # @user = User.new(user_params)
+    # if @user.save
+    #   render json: { :user => @user }, status: :created
+    # else
+    #   render json: { :messages => @user.errors.full_messages }, status: :unprocessable_entity
+    # end
+
+    render json: { :message => "Not yet open to the public" }, status: :ok
   end
 
   private
