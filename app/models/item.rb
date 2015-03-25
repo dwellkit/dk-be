a0ff68d8622102991a14e50cceebd888ffa5f01f
Dylan Dechant <dylan.dechant@gmail.com>
2015-03-25 10:24:04 -0400

    warranty associated with item now destroyed with the item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   has_many :contacts, as: :reachable
   has_many :reminders
-  has_many :warranties
+  has_many :warranties, dependent: :destroy
   has_many :insurances
   has_many :pictures, as: :picturable
   belongs_to :room
